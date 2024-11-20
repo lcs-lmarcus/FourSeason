923fcccff007ca37017c7ba429c515e494834a0c
Kyle Xiang <cxiang@lcs.on.ca>
2024-11-20 10:32:10 -0500

    Initial Commit

---
+++ FourSeason/FourSeasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourSeasonApp.swift
+//  FourSeason
+//
+//  Created by Chujun Xiang on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct FourSeasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
